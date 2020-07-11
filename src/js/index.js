aade4ba33baa383662747c86cfbf9faff2350874
devacran <migueeldrg@gmail.com>
2020-07-11 15:45:40 -0500

    Proyecto terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,47 @@
 const $catalog = document.getElementById("catalog");
 const $catalogNavigation = document.getElementById("catalogNavigation");
-const $catalogNavigationButtons = Array.from(
-  $catalogNavigation.querySelectorAll("a")
+const catalogControls = Array.from($catalogNavigation.querySelectorAll("a"));
+
+const $bookSlider = document.getElementById("bookSlider");
+const bookSliderControls = Array.from(
+  document.querySelectorAll(".bookSlider__control")
 );
 
-const makeNavigationListeners = ($element, controls, movementAmount) => {
-  controls.map((control, controlID) => {
-    control.addEventListener("click", () => {
-      moveElement($element, movementAmount, controlID);
-    });
-  });
-};
-const moveElement = ($element, movementAmount, controlID) => {
-  // const controlsQty = $element.length;
-  // const maxMovement = $element.length * movementAmount;
-  const position = controlID * movementAmount;
-  move(position);
-};
+const $testimonialSlider = document.getElementById("testimonialSlider");
+const testimonialSliderControls = Array.from(
+  document.querySelectorAll(".testimonialSlider__control")
+);
 
-const move = (position) => {
-  $catalog.style.transform = `translateX(-${position}px)`;
-};
+class MoveElement {
+  constructor($element, movementAmount) {
+    this.element = $element;
+    this.movementAmount = movementAmount;
+  }
+  moveElement(controlID) {
+    const position = controlID * this.movementAmount;
+    this.element.style.transform = `translateX(-${position}px)`;
+  }
+}
+
+class MoveElementControl extends MoveElement {
+  constructor($element, controls, movementAmount) {
+    super($element, movementAmount);
+    this.controls = controls;
+    this.makeControlsListeners();
+  }
+  makeControlsListeners() {
+    this.controls.map((control, controlID) => {
+      control.addEventListener("click", () => {
+        this.moveElement(controlID);
+      });
+    });
+  }
+}
+
+const catalog = new MoveElementControl($catalog, catalogControls, 1000);
+const testimonialSlider = new MoveElementControl(
+  $testimonialSlider,
+  testimonialSliderControls,
+  600
+);
+const bookSlider = new MoveElementControl($bookSlider, bookSliderControls, 600);
